e75a12eb32a33a1f308b929a5f266fcc6ed399be
sachii <sachinandan2121@gmail.com>
2024-08-09 21:05:35 +0530

    debug

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,11 @@
+#!/bin/bash
 
+# Install dependencies
+pip install -r requirements.txt
 
-pip install -r requirements.txt 
-python3.9 manage.py collectstatic
+# Run migrations (if needed)
+python manage.py migrate
 
+# Collect static files (if applicable)
+python manage.py collectstatic --noinput
 
