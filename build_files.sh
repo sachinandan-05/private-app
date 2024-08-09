8a35b5e5171d5138cc7c5a196fc8a3e2539118c4
Sachinandan <112381035+sachinandan-05@users.noreply.github.com>
2024-08-09 21:06:40 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+# Install dependencies
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+
+# Run migrations (if needed)
 python manage.py migrate
+
+# Collect static files (if applicable)
+python manage.py collectstatic --noinput
+
